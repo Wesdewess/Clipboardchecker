75da0324e2c4c8590013c0312da0535c1ccd1d2d
Wesdewess <35997013+Wesdewess@github.com>
2018-03-15 13:45:36 +0100

    first stage

---
+++ src/clipboardapplication/Clipboardapplication.java
@@ -0,0 +1,124 @@
+package clipboardapplication;
+
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.Toolkit;
+import java.io.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Wessel
+ */
+public final class Clipboardapplication implements ClipboardOwner {
+
+    public static void main(String... aArguments) {
+        Clipboardapplication textTransfer = new Clipboardapplication();
+        int tracking = 1;
+        String previousContent = null;
+        String currentContent;
+        
+        //display what is currently on the clipboard
+        //System.out.println("Clipboard contains:\n" + textTransfer.getClipboardContents());
+        currentContent = textTransfer.getClipboardContents();
+        
+        //Properties of the main JFrame
+        
+        JFrame mainWindow = new JFrame();
+        final int defaultFrameWidth = 500;
+        final int defaultFrameHeight = 600;
+        mainWindow.setSize(defaultFrameWidth, defaultFrameHeight);
+        mainWindow.setTitle("Clipboard Content Tracker @Wessel Bakker - 2018");
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel mainPanel = new JPanel();
+        JLabel currentDisplay = new JLabel("");
+        JLabel previousDisplay = new JLabel("");
+        JLabel topInfo = new JLabel("Current content of system clipboard(updating may take a few seconds):");
+        JLabel previousInfo = new JLabel("This was the previous content of the clipboard:");
+        mainPanel.add(topInfo);
+        mainPanel.add(currentDisplay);
+        mainPanel.add(previousInfo);
+        mainPanel.add(previousDisplay);
+        mainWindow.add(mainPanel);
+        
+        
+        //display the main window
+        mainWindow.setVisible(true);
+        
+        while (tracking == 1) {
+            try{
+                currentContent = textTransfer.getClipboardContents();
+            }
+            catch(IllegalStateException meh){
+            }
+
+            if (!currentContent.equals(previousContent)) {
+                System.out.println("Clipboard contains:\n" + currentContent);
+                currentDisplay.setText(currentContent);
+                previousDisplay.setText(previousContent);
+                previousContent = textTransfer.getClipboardContents();
+                System.out.println("previous content is now set to:\n" + previousContent);
+            } else {
+
+            }
+        }
+
+        //change the contents and then re-display
+        /*
+    textTransfer.setClipboardContents("blah, blah, blah");
+    System.out.println("internal clipboard contains:" + textTransfer.getClipboardContents());
+         */
+    }
+
+    /**
+     * Empty implementation of the ClipboardOwner interface.
+     * @param aClipboard
+     * @param aContents
+     */
+    @Override
+    public void lostOwnership(Clipboard aClipboard, Transferable aContents) {
+        //do nothing
+    }
+
+    /**
+     * Place a String on the clipboard, and make this class the owner of the
+     * Clipboard's contents.
+     * @param aString
+     */
+    public void setClipboardContents(String aString) {
+        StringSelection stringSelection = new StringSelection(aString);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, this);
+    }
+
+    /**
+     * Get the String residing on the clipboard.
+     *
+     * @return any text found on the Clipboard; if none found, return an empty
+     * String.
+     */
+    public String getClipboardContents() {
+        String result = "";
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+        //odd: the Object param of getContents is not currently used
+        Transferable contents = clipboard.getContents(null);
+        boolean hasTransferableText
+                = (contents != null)
+                && contents.isDataFlavorSupported(DataFlavor.stringFlavor);
+        if (hasTransferableText) {
+            try {
+                result = (String) contents.getTransferData(DataFlavor.stringFlavor);
+            } catch (UnsupportedFlavorException | IOException ex) {
+                System.out.println(ex);
+            }
+        }
+        return result;
+    }
+}
